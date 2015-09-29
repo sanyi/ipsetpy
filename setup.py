c71d823d94b375fcad8dde99e0b22686b980d938
Sandor Attila Gerendi <sanyi@users.noreply.github.com>
2015-09-29 11:16:49 +0300

    updated pypi version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(name='ipsetpy',
       license=LICENSE,
-      version='v0.0.1a1',
+      version='v0.0.1a2',
       description='Python ipset bindings and helper',
       long_description=README,
       author='Sandor Attila Gerendi',
